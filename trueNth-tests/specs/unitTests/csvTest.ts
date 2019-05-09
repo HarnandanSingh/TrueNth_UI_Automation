e7384a2ce5f49d6deb940e45ee6b82e319712850
Harnandan Singh <harnandan.singh@movember.com@Harnandans-MacBook-Pro.local>
2019-05-09 10:56:44 +1000

    Refactor

--- trueNth-tests/specs/unitTests/csvTest.ts
+++ trueNth-tests/specs/unitTests/csvTest.ts
@@ -5,6 +5,6 @@ import { mappings } from '../../dataFileMappings';
 describe('Verify results for selected filters', function () {
 
     it('Should display the correct articles list', async function() {
-        console.log(parseCsv("./test/dataFile.csv", mappings));
+        console.log(parseCsv("./trueNth-tests/dataFile.csv", mappings));
     });
 });
