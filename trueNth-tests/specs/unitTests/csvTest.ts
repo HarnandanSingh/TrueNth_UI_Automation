3f8aab09997fc8ba7ed84e2a5496cec278587ac9
Harnandan Singh <harnandan.singh@movember.com@Harnandans-MacBook-Pro.local>
2019-05-12 09:03:13 +1000

    Add assertion to check the length of the parsed results

--- trueNth-tests/specs/unitTests/csvTest.ts
+++ trueNth-tests/specs/unitTests/csvTest.ts
@@ -2,11 +2,16 @@
 import { parseCsv } from '../../../src/utilities/parser';
 import { map } from '../../dataFileMappings';
 import * as fs from 'fs';
+import * as assert from 'assert';
 
 describe('Verify results for selected filters', function () {
 
-    it('Should display the correct articles list', async function() {
+    it('Should have the correct number of records', async function() {
         const parsed = parseCsv("./trueNth-tests/dataFile.csv");
-        fs.writeFileSync('./parsedJson.json', map(parsed));
+        // fs.writeFileSync('./parsedJson.json', map(parsed));
+
+        const results: [] = JSON.parse(map(parsed));
+        console.log("NUMBER OF RESULTS: ********* " + results.length);
+        assert.equal(1008, results.length, "LENGTH DID NOT MATCH");
     });
 });
