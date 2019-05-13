73a3ed0d7c8a9963c7ed98318614170214e8b239
Harnandan Singh <harnandan.singh@movember.com@Harnandans-MacBook-Pro.local>
2019-05-13 10:21:01 +1000

    Remove redundant code

--- trueNth-tests/specs/unitTests/csvTest.ts
+++ trueNth-tests/specs/unitTests/csvTest.ts
@@ -7,11 +7,13 @@ import * as assert from 'assert';
 describe('Verify results for selected filters', function () {
 
     it('Should have the correct number of records', async function() {
+
         const parsed = parseCsv("./trueNth-tests/dataFile.csv");
         // fs.writeFileSync('./parsedJson.json', map(parsed));
 
         const results: [] = JSON.parse(map(parsed));
         console.log("NUMBER OF RESULTS: ********* " + results.length);
         assert.equal(1008, results.length, "LENGTH DID NOT MATCH");
+        
     });
 });
