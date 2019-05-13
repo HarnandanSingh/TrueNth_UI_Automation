dfda427920a85f3fc941e2d8abb67351ae59b507
Harnandan Singh <harnandan.singh@movember.com@Harnandans-MacBook-Pro.local>
2019-05-13 10:30:21 +1000

    Add comments

--- trueNth-tests/api.ts
+++ trueNth-tests/api.ts
@@ -1,2 +1,15 @@
 
-export const Articles = (lifeChange = '', treatment = '') => `https://truenth.org/en-gb/api/articles/?life_change=${lifeChange}&treatment=${treatment}`;
+/*
+    Articles function - takes inputs to construct an endpoint
+    and returns the endpoint to the caller.
+*/
+export const Articles = (lifeChange = '', treatment = '') => 
+                            `https://truenth.org/en-gb/api/articles/?life_change=${lifeChange}&treatment=${treatment}`;
+
+
+/*
+    Create functions to construct other endpoints in the same way as the Articles funciton.
+
+    Not all endpoints may need inputs, in which case just return the endpoint as is, but make
+    sure to give the function a meaningful name i.e. it should represent what the api endpoint is for.
+*/
